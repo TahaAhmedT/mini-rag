7c1b5d7a85da1ef25cb72e8ef86c509091f88ff2
TahaAhmedT <tahaahmedghoba4y@gmail.com>
2025-11-18 22:01:53 +0200

    Rename _id to be id and create an alias for it "_id"

--- src/models/db_schemes/project.py
+++ src/models/db_schemes/project.py
@@ -3,7 +3,7 @@ from typing import Optional
 from bson.objectid import ObjectId
 
 class Project(BaseModel):
-    _id: Optional[ObjectId]
+    id: Optional[ObjectId] = Field(None, alias="_id")
     project_id: str = Field(..., min_length=1)
 
     @validator('project_id')
@@ -13,5 +13,5 @@ class Project(BaseModel):
         
         return value
     
-    class config:
+    class Config:
         arbitrary_types_allowed = True
