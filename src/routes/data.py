f4682c826af41972d64feed13d4425ef001cb287
TahaAhmedT <tahaahmedghoba4y@gmail.com>
2025-11-19 21:34:42 +0200

    Add asset collection to mongodb

--- src/routes/data.py
+++ src/routes/data.py
@@ -9,8 +9,11 @@ import logging
 from .schemes.data import ProcessRequest
 from controllers import ProcessController
 from models.ProjectModel import ProjectModel
-from models.db_schemes import DataChunk
+from models.db_schemes import DataChunk, Asset
+from models.AssetModel import AssetModel
 from models.ChunkModel import ChunkModel
+from models.enums.AssetTypeEnum import AssetTypeEnum
+from bson import ObjectId
 
 logger = logging.getLogger("uvicorn.error")
 
@@ -63,10 +66,24 @@ async def upload_data(request: Request, project_id: str, file: UploadFile,
             }
         )
     
+    # store the assets into the database
+    asset_model = await AssetModel.create_instance(
+        db_client=request.app.db_client
+    )
+
+    asset_resource = Asset(
+        asset_project_id=ObjectId(project.id),
+        asset_type=AssetTypeEnum.FILE.value,
+        asset_name=file_id,
+        asset_size=os.path.getsize(file_path)
+    )
+
+    asset_record = await asset_model.create_asset(asset=asset_resource)
+    
     return JSONResponse(
             content={
                 "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
-                "file_id": file_id
+                "file_id": str(asset_record.id),
             }
         )
 
